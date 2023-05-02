541f890c4034190e20628c7aa77294da751aebd3
Akif GÜrkan <akifgurkan@hotmail.com>
2023-05-02 16:28:58 +0300

    22. Günsonu

--- src/day22_stringBuilder_accessModifier/C05_accessModifier.java
+++ src/day22_stringBuilder_accessModifier/C05_accessModifier.java
@@ -1,34 +1,44 @@
 package day22_stringBuilder_accessModifier;
 
 public class C05_accessModifier {
-
-
-    C05_accessModifier(){
+    public C05_accessModifier() {
     }
+
     private String privateStr;
     String defaultStr;
     protected String protectedStr;
     public String publicStr;
     private static int privateStaticSayi;
     static int defaultStaticSayi;
+
     protected static int protectedStaticSayi;
+
     public static int publicStaticSayi;
-    private void privateMethod(){
+
+    private void privateMethod() {
     }
-    void defaultMethod(){
+
+    void defaultMethod() {
     }
-    protected void protectedMethod(){
+
+    protected void protectedMethod() {
     }
-    public void publicMethod(){
+
+    public void publicMethod() {
     }
-    private static void privateStaticMethod(){
+
+    private static void privateStaticMethod() {
     }
-    static void defaultStaticMethod(){
+
+    static void defaultStaticMethod() {
     }
-    protected static void protectedStaticMethod(){
+
+    protected static void protectedStaticMethod() {
     }
-    public static void publicStaticMethod(){
+
+    public static void publicStaticMethod() {
     }
+
     public static void main(String[] args) {
         // tum static class uyeleri direk kullanilabilir
         // static olmayan tum class uyeleri icin ise obje lazim
@@ -39,7 +49,8 @@ public class C05_accessModifier {
         // static class uyelerini obje uzerinden kullanmaya gerek yoktur
         // ama kullanmak istersek java itiraz etmez
     }
-    public void classUyeKullanimi(){
+
+    public void classUyeKullanimi() {
         // static olan veya olmayan tum class uyeleri kullanilabilir
         System.out.println(privateStaticSayi);
         publicStaticMethod();
